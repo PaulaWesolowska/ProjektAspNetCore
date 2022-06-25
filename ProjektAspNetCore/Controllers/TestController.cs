af701b0b9fc949ed5b0ee1609e0d06c00710c2e7
paula <paula.anna.wesolowska@gmail.com>
2022-06-25 10:05:26 +0200

    Dodaj pliki projektów.

---
+++ ProjektAspNetCore/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjektAspNetCore.Models;
+
+namespace ProjektAspNetCore.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new TestModel();
+            model.Imie = "Paula";
+            model.Nazwisko = "Wesołowska";
+            return View(model);
+        }
+    }
+}
---
+++ ProjektAspNetCore/Models/TestModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace ProjektAspNetCore.Models
+{
+    public class TestModel
+    {
+        public string? Imie { get; set; }
+        public string? Nazwisko { get; set; }
+    }
+}
